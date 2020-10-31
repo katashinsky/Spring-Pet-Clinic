235d147727c8e8d023bb17e124a7360a75a07377
Maks <Katashinskydmitry@gmail.com>
2020-10-31 14:56:04 +0200

    split to multi module project

---
+++ pet-clinic-data/src/main/java/com/example/springpetclinic/MainClass.java
@@ -0,0 +1,10 @@
+package com.example.springpetclinic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+public class MainClass {
+
+    public static void main(String[] args) { }
+
+}
