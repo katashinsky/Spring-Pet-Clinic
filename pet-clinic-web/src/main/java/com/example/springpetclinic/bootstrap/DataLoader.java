4e0352373c2c71c29e078e7754c7e35d3d7f8c5d
Maks <Katashinskydmitry@gmail.com>
2020-11-02 12:39:19 +0200

    made some refactoring

--- pet-clinic-web/src/main/java/com/example/springpetclinic/bootstrap/DataLoader.java
+++ pet-clinic-web/src/main/java/com/example/springpetclinic/bootstrap/DataLoader.java
@@ -20,13 +20,11 @@ public class DataLoader implements CommandLineRunner {
     @Override
     public void run(String... args) throws Exception {
         Owner owner1 = new Owner();
-        owner1.setId(1L);
         owner1.setFirstName("first Owner1");
         owner1.setLastName("last Owner1");
         ownerService.save(owner1);
 
         Owner owner2 = new Owner();
-        owner1.setId(2L);
         owner1.setFirstName("first Owner2");
         owner1.setLastName("last Owner2");
         ownerService.save(owner2);
@@ -34,13 +32,11 @@ public class DataLoader implements CommandLineRunner {
         System.out.println("Loaded owners ... ");
 
         Vet vet1 = new Vet();
-        vet1.setId(1L);
         vet1.setFirstName("first Vet1");
         vet1.setLastName("last Vet1");
         vetService.save(vet1);
 
         Vet vet2 = new Vet();
-        vet2.setId(2L);
         vet2.setFirstName("first Vet2");
         vet2.setLastName("last Vet2");
         vetService.save(vet2);
