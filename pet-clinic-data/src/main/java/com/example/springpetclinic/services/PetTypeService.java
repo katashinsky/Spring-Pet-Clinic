e3522ff0f7a19e5ebe97a8cabc40bd216eab8565
Maks <Katashinskydmitry@gmail.com>
2020-11-04 15:54:04 +0200

    add pet type service

---
+++ pet-clinic-data/src/main/java/com/example/springpetclinic/services/PetTypeService.java
@@ -0,0 +1,9 @@
+package com.example.springpetclinic.services;
+
+import com.example.springpetclinic.model.PetType;
+
+import java.util.Set;
+
+public interface PetTypeService extends GrudService<PetType, Long>{
+
+}
