1a38fb02decd1b0c25584c2ad7cf8257e8024e42
Maks <Katashinskydmitry@gmail.com>
2020-11-07 12:16:48 +0200

    add JPA entities

--- pet-clinic-data/src/main/java/com/example/springpetclinic/model/Person.java
+++ pet-clinic-data/src/main/java/com/example/springpetclinic/model/Person.java
@@ -1,7 +1,18 @@
 package com.example.springpetclinic.model;
 
+import com.sun.istack.NotNull;
+
+import javax.persistence.Column;
+import javax.persistence.MappedSuperclass;
+
+@MappedSuperclass
 public class Person extends BaseEntity{
+
+    @Column(name = "first_name")
+    @NotNull
     private String firstName;
+
+    @Column(name = "last_name")
     private String lastName;
 
     public String getFirstName() {
